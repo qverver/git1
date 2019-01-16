3904b4c039e778e62bb91feed6a03ab4d2da66b3
qverver <qverver@gmail.com>
2019-01-16 10:51:52 +0200

    16.01.2019 v1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('col-lg-2 d-lg-block');
+                $('#main').toggleClass('col-lg-10');
+            });
+            $('#sidebarCollapse2').on('click', function () {
+                $('#sidebar').toggleClass('mobact');
+            });
